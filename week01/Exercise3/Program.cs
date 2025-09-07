8d49f223a8755f4986ad0f2da17b44df3ea09adf
tennarow <tennaqueen@gmail.com>
2025-09-06 17:30:10 -0700

    exercise 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -5,5 +5,46 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
+
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 100);
+
+
+        Console.Write("What is your guess? ");
+        string answer = Console.ReadLine();
+        int guess = int.Parse(answer);
+
+        if (guess < magicNumber)
+        {
+            Console.WriteLine("Too low!");
+        }
+        else if (guess > magicNumber)
+        {
+            Console.WriteLine("Too high!");
+        }
+        else
+        {
+            Console.WriteLine("You guessed it! What are the odds?!?");
+        }
+        
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            answer = Console.ReadLine();
+            guess = int.Parse(answer);
+
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Too low!");
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Too high!");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it! What are the odds?!?");
+            }
+        }
     }
 }
