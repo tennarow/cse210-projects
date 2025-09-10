9370ce7ad2b5084216df19373be1717b13627c0e
tennarow <tennaqueen@gmail.com>
2025-09-10 13:49:18 -0700

    activity week 2

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -1,9 +1,35 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._company = "Microsoft";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 2020;
+        job1._endYear = 2023;
+        job1.DisplayJobDetails();
+
+        job1.DisplayJobDetails();
+
+        Job job2 = new Job();
+        job2._company = "Google";
+        job2._jobTitle = "Senior Developer";
+        job2._startYear = 2023;
+        job2._endYear = 2024;
+        job2.DisplayJobDetails();
+
+        job2.DisplayJobDetails();
+
+        Resume myResume = new Resume();
+        myResume._name = "John Doe";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+        myResume.DisplayResume();
+    
+        
     }
+
 }
